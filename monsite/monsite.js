2d84f0e7d7c00a895151f95dfb067e1d73edf514
ahmadoubambasow <97096629+ahmadoubambasow@users.noreply.github.com>
2022-01-04 12:09:22 +0100

    Add files via upload

---
+++ monsite/monsite.js
@@ -0,0 +1,73 @@
+var nomentreprise=document.getElementById('nomentreprise')
+    nomentreprise.addEventListener('mousemove',function(){
+    	nomentreprise.innerHTML="Bienvenue chez MLS_GLOBAL_BUSINESS";
+    	nomentreprise.style.color="yellow";
+    });
+
+var nomentreprise=document.getElementById('nomentreprise')
+    nomentreprise.addEventListener('mouseout',function(){
+        nomentreprise.innerHTML="MLS_GLOBAL_BUSINESS";
+    	nomentreprise.style.color="";
+    })
+
+var produit1=document.getElementById('produit1')
+    produit1.addEventListener('mousemove',function(){
+    	produit1.style.color='yellow'
+    })   
+
+var produit1=document.getElementById('produit1')
+    produit1.addEventListener('mouseout',function(){
+    	produit1.style.color="";
+    })   
+
+var produit2=document.getElementById('produit2')
+    produit2.addEventListener('mousemove',function(){
+    	produit2.style.color='red'
+    })
+
+var produit2=document.getElementById('produit2')
+    produit2.addEventListener('mouseout',function(){
+    	produit2.style.color="";
+    })
+var multi=document.getElementById('multi')
+    multi.addEventListener('mousemove',function(){
+    	multi.style.color='orange'
+    })
+
+var multi=document.getElementById('multi')
+    multi.addEventListener('mouseout',function(){
+    	multi.style.color="";
+    })
+var p1=document.getElementById('p1')
+    p1.addEventListener('click',function(){
+        p1.style.color="white";
+    });
+
+var p2=document.getElementById('p2')
+    p2.addEventListener('mousemove',function () {
+    	p2.style.color='teal';
+    });
+
+var p3=document.getElementById('p3')
+    p3.addEventListener('mousemove',function(){
+    	p3.style.color='yellow';
+    });
+
+var p4=document.getElementById('p4')
+    p4.addEventListener('mousemove',function(){
+    	p4.style.color='red';
+    });
+
+var p5=document.getElementById('p5')
+    p5.addEventListener('mousemove',function(){
+    	p5.style.color='orange';
+    });
+
+var contact=document.getElementById('contact')
+    contact.addEventListener('mousemove',function(){
+    	contact.innerHTML="Tel:77-571-16-51"
+    })
+var contact=document.getElementById('contact')
+    contact.addEventListener('mouseout',function(){
+    	contact.innerHTML="contact";
+    })
